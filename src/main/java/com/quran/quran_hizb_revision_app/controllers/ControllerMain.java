d306e827e948421e461119f96d859517a11f5ff6
Mimouni-Amine <amine.m1039@gmail.com>
2023-07-09 12:08:22 +0200

    feat: added Cross Origin to solve Front fetch problem

--- src/main/java/com/quran/quran_hizb_revision_app/controllers/ControllerMain.java
+++ src/main/java/com/quran/quran_hizb_revision_app/controllers/ControllerMain.java
@@ -7,14 +7,12 @@ import com.quran.quran_hizb_revision_app.dao.IDAORevision;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.Date;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1")
 public class ControllerMain {
@@ -26,6 +24,7 @@ public class ControllerMain {
     ControllerMain (IDAORevision idaoRevision){
         this.idaoRevision = idaoRevision;
     }
+
 
 
 
