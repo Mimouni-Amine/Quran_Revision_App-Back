52b3ad6ff1955f4ef03e99e59827fec8eeecf1b2
Mimouni-Amine <amine.m1039@gmail.com>
2023-12-03 18:53:45 +0100

    feat : add swagger and findAllUsers method

--- src/main/java/com/quran/quran_hizb_revision_app/dao/IDAOUser.java
+++ src/main/java/com/quran/quran_hizb_revision_app/dao/IDAOUser.java
@@ -1,10 +1,13 @@
 package com.quran.quran_hizb_revision_app.dao;
 
 import com.quran.quran_hizb_revision_app.Entities.User;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IDAOUser extends JpaRepository<User, Integer> {
 
   public Optional<User> findById(Integer userId);
+
+  public List<User> findAllByOrderByUserName();
 }
